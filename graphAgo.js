537656cae674d68e86a2232d2e1bec8bd061dfe2
vannesteromain <romain.vanneste.etu@univ-lille.fr>
2021-01-26 15:12:40 +0100

    Début de la partie ago

---
+++ graphAgo.js
@@ -0,0 +1,25 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'pie',
+
+    // The data for our dataset
+    data: {
+        labels: ['Voie', 'Signalisation', 'Caténaire', 'Produit Train', 'Circulation'],
+        datasets: [{
+            data: [35, 25, 8, 12, 20],
+            backgroundColor : ["#0088ce","#e05206","#82be00", "#a1006b", "#009aa6"]
+        }]
+    },
+
+    // Configuration options go here
+    options: {responsive: true,
+        title: {
+            display: true,
+            text: 'Répartition des sessions'
+        },
+        legend :{
+            position : 'bottom'
+        }
+    }
+});
